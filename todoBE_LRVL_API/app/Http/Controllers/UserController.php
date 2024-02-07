e5a4be9aabb8ddd574a7269920a0fd8f7352a625
kadirtikil <kadirtikil@gmail.com>
2024-02-07 01:31:56 +0100

    Controller register is done

--- todoBE_LRVL_API/app/Http/Controllers/UserController.php
+++ todoBE_LRVL_API/app/Http/Controllers/UserController.php
@@ -12,16 +12,36 @@ class UserController extends Controller
     // User related methods
     public function login(Request $request){
         // Sanitize data to prevent forgery
+
+        if(!User::find($request->email)){
+            return response()->json(['message' => 'User not found tho']);
+        }
+
         return response()->json(['message' => $request]);
     }   
 
     public function register(Request $request){
-        // Sanitize data to prevent forgery
-        
-        
-        $user = new User;
+        // get credentials out of request body
+        $creds = $request->validate([
+            'role' => 'string|max:255',
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|max:255',
+            'phone' => 'string|max:255',
+        ]);
 
-        $creds = $request->only('email', 'name', 'password');
+
+        // check if mail already exists in DB
+        // couldve checked with unique:users in the validation above, but somehow it lead to the welcome blade file.
+        // im guessing thats eloquent/illuminate jumping in.
+        if (User::where('email', $creds['email'])->exists()) {
+            return response()->json(['message' => 'User already exists']);
+        }
+
+
+        // csrf token already set in form , no need to sanitize request
+        // using eloquent model to save user, no need to check for injection
+        $user = new User;
 
         $user->email = $creds['email'];
         $user->username = $creds['name'];
@@ -33,7 +53,7 @@ class UserController extends Controller
         $user->save();
 
         // Testing with postman if entered request data is reaching the backend. worked
-        return response()->json(['message' =>'reached backend']);
+        return response()->json(['message' => 'User registered']);
     }
 
     public function logout(){
