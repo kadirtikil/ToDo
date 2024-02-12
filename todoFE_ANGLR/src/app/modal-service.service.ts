2ce32d6c16ca5b4872c4e008d0442335f06e2911
kadirtikil <kadirtikil@gmail.com>
2024-02-12 17:06:22 +0100

    Started creating the modals/dialog using angular/material. Will continue to embed everything and setup the connection to api with the service

---
+++ todoFE_ANGLR/src/app/modal-service.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import MatDialogModule from '@angular/material/dialog';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalServiceService {
+
+  constructor() { }
+}
--- todoFE_ANGLR/src/app/navigation/navigation.component.ts
+++ todoFE_ANGLR/src/app/navigation/navigation.component.ts
@@ -1,4 +1,13 @@
+import { DialogConfig } from '@angular/cdk/dialog';
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
+import { CreateTasksComponent } from '../create-tasks/create-tasks.component';
+import { SigninComponent } from '../signin/signin.component';
+import { SignupComponent } from '../signup/signup.component';
+
+import { MyApiService } from '../my-api.service';
+
 
 @Component({
   selector: 'app-navigation',
@@ -8,5 +17,40 @@ import { Component } from '@angular/core';
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+  createTask(){
+    console.log('createTask');
+  }
 
+  constructor(private dialog: MatDialog){}
+
+  openCreateTaskDialog(){
+    const dialogconfig = new MatDialogConfig();
+
+    dialogconfig.disableClose = true;
+    dialogconfig.autoFocus = true;
+
+    this.dialog.open(CreateTasksComponent)
+  }
+
+  openSignInDialog(){
+    const dialogconfig = new MatDialogConfig();
+
+    dialogconfig.disableClose = true;
+    dialogconfig.autoFocus = true;
+
+    this.dialog.open(SigninComponent)
+  }
+
+  openSignUpDialog(){
+    const dialogconfig = new MatDialogConfig();
+
+    dialogconfig.disableClose = true;
+    dialogconfig.autoFocus = true;
+
+    this.dialog.open(SignupComponent)
+  }
+
+  logout(){
+    console.log("logout")
+  }
 }
